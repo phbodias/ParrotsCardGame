26104bd371b1b752903f558978b3d7e3b73bdf9f
Pedro Dias <pedrohenriquebritooliveira@gmail.com>
2022-04-10 21:17:58 -0300

    second commit - basic

--- scripts.js
+++ scripts.js
@@ -1,19 +1,54 @@
+/* Funções */
+
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+
 function addcards(n){
-    const cards = document.querySelector(".cards");
-    let i = 0;
-    while ( i < n){
+    let usedcards = backs.slice(0, (n/2));
+    let cards = document.querySelector("ul");
+    let contador = 0;
+    for (let i = 0; i < n; i++){
         cards.innerHTML += `
-            <div class="card">
-                <div class="front-face face">Frente</div>
-                <div class="back-face face">Verso</div>
-            </div>
+            <ul class="card" onClick="flip(this)">
+                <li class="face"><img src="/images/front.png"></li>
+                <li class="back-face face"><img src="${usedcards[contador]}"></li>
+            </ul>
         `
-        i++;
+        if (contador === usedcards.length - 1){
+            contador = 0;
+            usedcards = usedcards.sort(comparador);
+        }else{
+            contador++;
+        }
     }
 }
 
+function flip(card){
+    
+}
+
+/*---------------------------------------------------------------------------------- */
+
+/* Variáveis globais */
+
 let number = prompt("Selecione o número de cartas (nº par entre 4 - 14)");
+
 while (number % 2 !== 0 || number < 4 || number >14){
     number = prompt("Selecione o número de cartas (nº par entre 4 - 14)");
 }
+
+let backs = [
+    "/images/bobrossparrot.gif",
+    "/images/explodyparrot.gif",
+    "/images/fiestaparrot.gif",
+    "/images/metalparrot.gif",
+    "/images/revertitparrot.gif",
+    "/images/tripletsparrot.gif",
+    "/images/chicken.gif",
+]
+/*---------------------------------------------------------------------------------- */
+
+/* Invocação de funções */
+backs = backs.sort(comparador);
 addcards(number);
